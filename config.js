a6b28d9caca196a14189a3bb1494b38509577599
Jay <jaystance25@gmail.com>
2022-02-16 23:13:11 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,4 @@
+
+  export const nftmarketaddress = "0xe8651998510F3Ff8Cd2E989c5864c38090bE3356"
+  export const nftaddress = "0x93818E78bC953bF99D4853148cf4BE8eB2f81a15"
+  
---
+++ hardhat.config.js
@@ -0,0 +1,39 @@
+require("@nomiclabs/hardhat-waffle");
+const fs = require('fs');
+let secret = require("./secret")
+
+module.exports = {
+    // defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 1337
+        },
+        ropsten: {
+            url: secret.url,
+            accounts: [secret.key]
+        },
+        /*
+        mumbai: {
+          // Infura
+          // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
+          url: "https://rpc-mumbai.matic.today",
+          accounts: [privateKey]
+        },
+        matic: {
+          // Infura
+          // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+          url: "https://rpc-mainnet.maticvigil.com",
+          accounts: [privateKey]
+        }
+        */
+    },
+    solidity: {
+        version: "0.8.4",
+        // settings: {
+        //   optimizer: {
+        //     enabled: true,
+        //     runs: 200
+        //   }
+        // }
+    }
+};
