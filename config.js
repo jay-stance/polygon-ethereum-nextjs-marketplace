6f93c659c7f52fc8e4ebac1fcdbb2f12f0f49c69
Jay <jaystance25@gmail.com>
2022-02-17 17:07:32 +0100

    Updates jsonRPCProvider

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 
-  export const nftmarketaddress = "0xb8A036257Feb5bE28d54bD31e36B56f93C6B4082"
-  export const nftaddress = "0x440991ED917A88259B41784565380313a37Bd9e9"
-  
+  // export const nftmarketaddress = "0xb8A036257Feb5bE28d54bD31e36B56f93C6B4082"
+  // export const nftaddress = "0x440991ED917A88259B41784565380313a37Bd9e9"
+
+
+  export const nftmarketaddress = "0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0"
+  export const nftaddress = "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9"
